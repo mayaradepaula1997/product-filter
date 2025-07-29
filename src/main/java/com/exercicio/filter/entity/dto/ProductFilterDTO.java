815f0fb6d4b88333e1d841caf8cfd557c54d6933
Mayara <mayaradepaula1997>
2025-07-28 23:05:40 -0300

    ProductFilterDTO creation

---
+++ src/main/java/com/exercicio/filter/entity/dto/ProductFilterDTO.java
@@ -0,0 +1,9 @@
+package com.exercicio.filter.entity.dto;
+
+import java.math.BigDecimal;
+
+public record ProductFilterDTO(String name, Long priceMin, Long priceMax) {
+
+
+
+}
--- src/main/java/com/exercicio/filter/repository/ProductRepository.java
+++ src/main/java/com/exercicio/filter/repository/ProductRepository.java
@@ -2,6 +2,10 @@ package com.exercicio.filter.repository;
 
 import com.exercicio.filter.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository  extends JpaRepository <Product, Long>{
+public interface ProductRepository  extends JpaRepository <Product, Long> , JpaSpecificationExecutor<Product> {
 }
+
+
+
